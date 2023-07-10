6e996a22c0e735423c2e935ad3dba7f0f8b16d85
mehmetfatihdurna <mfd304603@gmail.com>
2023-07-10 14:06:44 +0300

    Javascript giriş

---
+++ js/tutorials.js
@@ -0,0 +1,116 @@
+//Single Comment
+/*
+Multiple Comment 1
+Multiple Comment 2
+*/
+// console.log("loglama");
+// console.info("Bilgilendirme");
+// console.warn("Uyarı");
+// console.error("Hata");
+
+// alert("pop-up bilgilendirme-1")
+// window.alert("pop-up bilgilendirme-2") //window en tepedeki obje
+
+// document.writeln("ekrana yazı yazıyorum")
+
+// Variable
+
+/** 
+var result = 4343;
+console.log(typeof result + " ==> " + result);
+
+var result = 43.43;
+console.log(typeof result + " ==> " + result);
+
+var result = true;
+console.log(typeof result + " ==> " + result);
+
+var result = "Js öğrenmek";
+console.log(typeof result + " ==> " + result);
+
+//Naming Convensition
+
+var deneme = "JS Data";
+console.log(deneme);
+
+var deneme44 = "JS Data";
+console.log(deneme44);
+
+var _$deneme44 = "JS Data";
+console.log(_$deneme44);
+
+var denemeVerisi = "JS Data";
+console.log(denemeVerisi);
+
+// var 44deneme = "JS Data" sayıyla başlayamazsın
+
+//whoisting
+
+result2 = 56;
+var result2;
+
+*/
+
+/////////////////////////////////////////////////
+
+// Operators
+
+// + - * ** / %
+// x++
+// x--
+// ++x
+// --x
+// = atama
+// == EŞİT (TüRÜNE BAKMA)
+// === EŞİT (TÜRÜNE BAK)
+// ! DEĞİL
+// && ve || veya
+
+
+/** 
+var number1 = 40;
+var number2 = 5;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 / number2);
+console.log(number1 * number2);
+console.log(number1 % number2);
+
+var userData = Number(prompt("Lütfen birşeyler  yazınız: "))
+console.error(userData + 50);
+*/
+
+// undefined
+var result;
+console.log(result);
+
+//NaN: Not a Number
+var result= "asd"/3;
+console.log(result);
+
+// isNaN
+var result = 4;
+console.log(isNaN(result)); // falsedır çünkü sayı çıkar...
+
+// infinity
+
+console.log(5/0);
+
+//Math
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.min(1,2999,234234,-1));
+console.log(Math.max(12,14,2,13,18));
+console.log(Math.sqrt(81));
+console.log(Math.abs(-31));
+console.log(Math.pow(2,5));
+console.log(Math.floor(3.9));
+console.log(Math.ceil(3.1));
+console.log(Math.round(6.4));
+console.log(Math.round(6.1));
+console.log(Math.sin(90));
+
+console.log(Math.round(Math.random()*9+1));
+
+
